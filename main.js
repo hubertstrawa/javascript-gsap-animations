20443d7c94dca34c00453e797af93a06d91096ab
Vinyljam <hubertstrawa@gmail.com>
2019-07-30 16:29:19 +0200

    adding initial files

---
+++ main.js
@@ -0,0 +1,36 @@
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('#logo');
+const hamburger = document.querySelector('.hamburger');
+const headline = document.querySelector('.headline');
+
+const tl = new TimelineMax();
+
+tl.fromTo(hero, 1, { height: '0%' }, { height: '80%', ease: Power2.easeInOut })
+  .fromTo(
+    hero,
+    1.2,
+    { width: '100%' },
+    { width: '80%', ease: Power2.easeInOut }
+  )
+  .fromTo(
+    slider,
+    1.2,
+    { x: '-100%' },
+    { x: '0%', ease: Power2.easeInOut },
+    '-=1.2'
+  )
+  .fromTo(
+      logo,
+      1,
+      {opacity: '0'},
+      {opacity: '1'},
+      '-=0.5'
+  )
+  .fromTo(
+    hamburger,
+    1,
+    {opacity: '0'},
+    {opacity: '1'},
+    '-=1'
+)
